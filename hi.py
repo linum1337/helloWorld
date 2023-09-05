d24d784e5ae5e9cb9476b652d34654bc1eb04c3c
linum1337 <57835692+linum1337@users.noreply.github.com>
2023-09-05 11:56:08 +0300

    Update hi.py
    updated class

--- hi.py
+++ hi.py
@@ -1,9 +1,10 @@
 class PrintHello:
-  def __init__():
+  def __init__(self, iter: str):
     self.iter_number = 4
 def print_cycle(self, iter_num: int):
   for i in range(iter_num):
     print("Hello, world!")
 
-hi = PrintHello()
+
+hi = PrintHello(10)
 hi.print_cycle(2)
